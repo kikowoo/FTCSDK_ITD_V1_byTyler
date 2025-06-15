8d8ec20202f7380d253927f7fe6dd7c57c0d6f88
Tyler-0000 <huang02364@gmail.com>
2025-06-14 21:00:48 -0700

    a

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/OpModes/TeleOp/RobotCentricTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/OpModes/TeleOp/RobotCentricTeleOp.java
@@ -8,6 +8,7 @@ import com.pedropathing.pathgen.Point;
 import com.pedropathing.util.Timer;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Gamepad;
 
 import org.firstinspires.ftc.teamcode.Robot.Structure.Library.PoseStorage;
 import org.firstinspires.ftc.teamcode.pedroPathing.constants.FConstants;
@@ -20,13 +21,15 @@ import org.firstinspires.ftc.teamcode.pedroPathing.constants.LConstants;
  * @version 2.0, 12/30/2024
  */
 
-@TeleOp(name = "Example Robot-Centric Teleop", group = "Examples")
+@TeleOp(name = "Robot-Centric Teleop", group = "Examples")
 public class RobotCentricTeleOp extends OpMode {
     private Follower follower;
+    private Path scoreBasket;
     private Timer pathTimer, actionTimer, opmodeTimer;
     private int pathState;
     private final Pose startPose = PoseStorage.CurrentPose;
     private final Pose scorePose = new Pose(18, 130, Math.toRadians(315));
+    private Gamepad currentGamepad1, previousGamepad1;
 
     /** This method is call once when init is played, it initializes the follower **/
     @Override
@@ -37,6 +40,9 @@ public class RobotCentricTeleOp extends OpMode {
         pathTimer = new Timer();
         opmodeTimer = new Timer();
         opmodeTimer.resetTimer();
+
+        Gamepad currentGamepad1 = new Gamepad();
+        Gamepad previousGamepad1 = new Gamepad();
     }
 
     /** This method is called continuously after Init while waiting to be started. **/
@@ -53,13 +59,8 @@ public class RobotCentricTeleOp extends OpMode {
     /** This is the main loop of the opmode and runs continuously after play **/
     @Override
     public void loop() {
-
-        /* Update Pedro to move the robot based on:
-        - Forward/Backward Movement: -gamepad1.left_stick_y
-        - Left/Right Movement: -gamepad1.left_stick_x
-        - Turn Left/Right Movement: -gamepad1.right_stick_x
-        - Robot-Centric Mode: true
-        */
+        previousGamepad1.copy(currentGamepad1);
+        currentGamepad1.copy(gamepad1);
 
         follower.setTeleOpMovementVectors(
                 -gamepad1.left_stick_y,
@@ -68,23 +69,23 @@ public class RobotCentricTeleOp extends OpMode {
                 true);
         follower.update();
 
-        if(gamepad1.a) {
-            Path scoreBasket;
+        if(currentGamepad1.a && !previousGamepad1.a) {
             scoreBasket = new Path(new BezierLine(new Point(follower.getPose()), new Point(scorePose)));
             scoreBasket.setLinearHeadingInterpolation(follower.getPose().getHeading(), scorePose.getHeading());
             setPathState(0);
-            switch (pathState) {
-                case 0:
-                    follower.followPath(scoreBasket);
-                    setPathState(1);
-                    break;
-                case 1:
-                    if (!follower.isBusy()) {
-                        setPathState(-1);
-                        PoseStorage.CurrentPose = follower.getPose();
-                    }
-                    break;
-            }
+        }
+
+        switch (pathState) {
+            case 0:
+                follower.followPath(scoreBasket);
+                setPathState(1);
+                break;
+            case 1:
+                if (!follower.isBusy()) {
+                    setPathState(-1);
+                    PoseStorage.CurrentPose = follower.getPose();
+                }
+                break;
         }
 
         /* Telemetry Outputs of our Follower */
