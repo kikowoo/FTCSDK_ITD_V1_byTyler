e3b6a363a2e722b25ca3b51fa67c0d7c69fb1f21
Tyler-0000 <huang02364@gmail.com>
2025-06-14 20:31:14 -0700

    a

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Robot/Structure/PoseStorage.java
+++
@@ -1,6 +0,0 @@
-package org.firstinspires.ftc.teamcode.pedroPathing.Robot.Structure;
-
-import com.pedropathing.localization.Pose;
-public class PoseStorage {
-    public static Pose CurrentPose = new Pose(0,0,0);
-}
