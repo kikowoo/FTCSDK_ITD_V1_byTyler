0333fdc7c7305a3051068a2912b4a34ee3e6af42
Tyler-0000 <huang02364@gmail.com>
2025-07-26 21:03:54 -0700

    a1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Structure/Hardware/BTRobotV1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Structure/Hardware/BTRobotV1.java
@@ -358,7 +358,7 @@ public class BTRobotV1 {
     public void TransferSample(){
         Setup_Deposit_Claw(true);
         Setup_Deposit_Arm(0.1);
-        Setup_Deposit_Wrist(0.25);
+        Setup_Deposit_Wrist(0.23);
         Setup_Vertical_Lift(0, 1.0);
     }
 }
