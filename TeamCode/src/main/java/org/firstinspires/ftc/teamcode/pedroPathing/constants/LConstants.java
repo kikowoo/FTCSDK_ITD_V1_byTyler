9567814ee467c1fa1437f4685e47469c2a3baf5f
Darius White <white_darius@yahoo.com>
2025-07-26 17:31:11 -0700

    ty;er is sigma a odometry tuned

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/constants/LConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/constants/LConstants.java
@@ -7,15 +7,15 @@ public class LConstants {
     static {
         ThreeWheelConstants.forwardTicksToInches = 0.0019890004972532307;
         ThreeWheelConstants.strafeTicksToInches = 0.0019720163702662274;
-        ThreeWheelConstants.turnTicksToInches = 0.0026;
-        ThreeWheelConstants.leftY = 4;
-        ThreeWheelConstants.rightY = -4;
-        ThreeWheelConstants.strafeX = -1;
-        ThreeWheelConstants.leftEncoder_HardwareMapName = "leftFront";
-        ThreeWheelConstants.rightEncoder_HardwareMapName = "rightFront";
-        ThreeWheelConstants.strafeEncoder_HardwareMapName = "leftRear";
-        ThreeWheelConstants.leftEncoderDirection = Encoder.REVERSE;
-        ThreeWheelConstants.rightEncoderDirection = Encoder.REVERSE;
+        ThreeWheelConstants.turnTicksToInches = 0.0022;
+        ThreeWheelConstants.leftY = 3.5;
+        ThreeWheelConstants.rightY = -3.5;
+        ThreeWheelConstants.strafeX = -2;
+        ThreeWheelConstants.leftEncoder_HardwareMapName = "leftRear";
+        ThreeWheelConstants.rightEncoder_HardwareMapName = "rightRear";
+        ThreeWheelConstants.strafeEncoder_HardwareMapName = "rightFront";
+        ThreeWheelConstants.leftEncoderDirection = Encoder.FORWARD;
+        ThreeWheelConstants.rightEncoderDirection = Encoder.FORWARD;
         ThreeWheelConstants.strafeEncoderDirection = Encoder.REVERSE;
 
     }
